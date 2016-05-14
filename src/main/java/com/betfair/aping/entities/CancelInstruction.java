dd30bd98486207f6ce89b330d657632e69aa8fd0
boolivar <boolivar@gmail.com>
2016-05-14 19:18:26 +0300

    CancelInstruction, ReplaceInstruction

--- src/main/java/com/betfair/aping/entities/CancelInstruction.java
+++ src/main/java/com/betfair/aping/entities/CancelInstruction.java
@@ -1,5 +1,25 @@
 package com.betfair.aping.entities;
 
+/** Instruction to fully or partially cancel an order (only applies to LIMIT orders) */
 public class CancelInstruction {
+	private String betId;
+	private double sizeReduction;
+	
+	public String getBetId() {
+		return betId;
+	}
+	
+	public void setBetId(String betId) {
+		this.betId = betId;
+	}
+	
+	/** If supplied then this is a partial cancel. Should be set to 'null' if no size reduction is required. */
+	public double getSizeReduction() {
+		return sizeReduction;
+	}
 
+	/** If supplied then this is a partial cancel. Should be set to 'null' if no size reduction is required. */
+	public void setSizeReduction(double sizeReduction) {
+		this.sizeReduction = sizeReduction;
+	}
 }
--- src/main/java/com/betfair/aping/entities/ReplaceInstruction.java
+++ src/main/java/com/betfair/aping/entities/ReplaceInstruction.java
@@ -1,5 +1,22 @@
 package com.betfair.aping.entities;
 
 public class ReplaceInstruction {
-
+	private String betId;
+	private double newPrice;
+	
+	public String getBetId() {
+		return betId;
+	}
+	
+	public void setBetId(String betId) {
+		this.betId = betId;
+	}
+	
+	public double getNewPrice() {
+		return newPrice;
+	}
+	
+	public void setNewPrice(double newPrice) {
+		this.newPrice = newPrice;
+	}
 }
