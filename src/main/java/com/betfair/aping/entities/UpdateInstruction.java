02b43cfe02fdc21bc2aa34a2737d06411a2105bb
boolivar <boolivar@gmail.com>
2016-05-14 19:14:55 +0300

    UpdateInstruction

--- src/main/java/com/betfair/aping/entities/UpdateInstruction.java
+++ src/main/java/com/betfair/aping/entities/UpdateInstruction.java
@@ -1,5 +1,27 @@
 package com.betfair.aping.entities;
 
-public class UpdateInstruction {
+import com.betfair.aping.enums.PersistenceType;
 
+/** Instruction to update LIMIT bet's persistence of an order that do not affect exposure */
+public class UpdateInstruction {
+	private String betId;
+	private PersistenceType newPersistenceType;
+	
+	public String getBetId() {
+		return betId;
+	}
+	
+	public void setBetId(String betId) {
+		this.betId = betId;
+	}
+	
+	/** The new persistence type to update this bet to */
+	public PersistenceType getNewPersistenceType() {
+		return newPersistenceType;
+	}
+	
+	/** The new persistence type to update this bet to */
+	public void setNewPersistenceType(PersistenceType newPersistenceType) {
+		this.newPersistenceType = newPersistenceType;
+	}
 }
