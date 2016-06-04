6886beff0bd3ec35a091d90646b31524c6608547
boolivar <boolivar@gmail.com>
2016-06-04 12:59:12 +0300

    EventTypeNames

---
+++ src/main/java/com/betfair/aping/entities/EventTypeNames.java
@@ -0,0 +1,33 @@
+package com.betfair.aping.entities;
+
+public class EventTypeNames {
+	public static final String AMERICAN_FOOTBALL = "American Football";
+	public static final String ATHLETICS = "Athletics";
+	public static final String BASEBALL = "Baseball";
+	public static final String BASKETBALL = "Basketball";
+	public static final String BOXING = "Boxing";
+	public static final String CHESS = "Chess";
+	public static final String CRICKET = "Cricket";
+	public static final String CYCLING = "Cycling";
+	public static final String DARTS = "Darts";
+	public static final String E_SPORTS = "E-Sports";
+	public static final String FINANCIAL_BETS = "Financial Bets";
+	public static final String GAELIC_GAMES = "Gaelic Games";
+	public static final String GOLF = "Golf";
+	public static final String GREYHOUND_RACING = "Greyhound Racing";
+	public static final String HANDBALL = "Handball";
+	public static final String HORSE_RACING = "Horse Racing";
+	public static final String ICE_HOCKEY = "Ice Hockey";
+	public static final String MIXED_MARTIAL_ARTS = "Mixed Martial Arts";
+	public static final String MOTOR_SPORT = "Motor Sport";
+	public static final String OLYMPICS_2016 = "Olympics 2016";
+	public static final String POKER = "Poker";
+	public static final String POLITICS = "Politics";
+	public static final String RUGBY_LEAGUE = "Rugby League";
+	public static final String RUGBY_UNION = "Rugby Union";
+	public static final String SNOOKER = "Snooker";
+	public static final String SOCCER = "Soccer";
+	public static final String SPECIAL_BETS = "Special Bets";
+	public static final String TENNIS = "Tennis";
+	public static final String VOLLEYBALL = "Volleyball";
+}
